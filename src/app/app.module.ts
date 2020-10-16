43b7d8458e12472014362531ea618d2de972a876
Diego Dos Santos <diegodossantos@MacBook-Pro.home>
2020-10-16 02:41:38 +0200

    Static page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
+import { Section3Component } from './components/section3/section3.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
